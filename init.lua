abc181ee37e8e256b55c342585821e9b1dffa0b8
Tadas Šimkus <tadas@furniture1.eu>
2024-07-03 09:36:01 +0300

    Center page on half page jump

--- init.lua
+++ init.lua
@@ -936,3 +936,6 @@ function _lazygit_toggle()
 end
 
 vim.api.nvim_set_keymap('n', '<leader>g', '<cmd>lua _lazygit_toggle()<CR>', { noremap = true, silent = true })
+
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
